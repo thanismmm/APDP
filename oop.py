4a2e681acc2074a1a015e6f4bc0adeac1d3389f8
Mohammed_Thanis <thanis7168t@gmail.com>
2024-01-08 19:57:35 +0530

    poly

--- oop.py
+++ oop.py
@@ -1,2 +1,22 @@
-print("Hello")
+class Students:
+   def __init__(self, name, rank, points):
+      self.name = name
+      self.rank = rank
+      self.points = points
 
+   # custom function
+   def demofunc(self):
+      print("I am "+self.name)
+      print("I got Rank ",+self.rank)
+
+# create 4 objects
+st1 = Students("Steve", 1, 100)
+st2 = Students("Chris", 2, 90)
+st3 = Students("Mark", 3, 76)
+st4 = Students("Kate", 4, 60)
+
+# call the functions using the objects created above
+st1.demofunc()
+st2.demofunc()
+st3.demofunc()
+st4.demofunc()
---
+++ polymorphism.py
@@ -0,0 +1,15 @@
+class Animal:
+    def sound(self):
+        print("Animal makes Sound")
+
+class Dog:
+    def sound(self):
+        print("Dogs hobs")
+
+class Bird:
+    def sound(self):
+        print("Bird is Singing")
+
+b1 = Bird()
+b1.sound()
+
